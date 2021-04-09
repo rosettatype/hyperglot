8ceb9b169c47f08c144ed5412c36f50678b3f883
Johannes Neumeier <hello@johannesneumeier.com>
2021-04-09 22:10:50 +0300

    Retain temporary mark refactor checks but only for Latin, Cyrillic and Greek

--- lib/hyperglot/validate.py
+++ lib/hyperglot/validate.py
@@ -72,7 +72,8 @@ def check_types(Langs):
                                   % iso)
 
                 # Temporary check to review mark refactor results
-                if "marks" in o and "inherit" not in o:
+                if "marks" in o and "inherit" not in o and \
+                        o["script"] in ("Latin", "Cyrillic", "Greek"):
                     marks_base = parse_marks(o["base"]) if "base" in o else []
                     marks_aux = parse_marks(o["auxiliary"]) if "auxiliary" in o else []  # noqa
                     marks = parse_marks(o["marks"])
