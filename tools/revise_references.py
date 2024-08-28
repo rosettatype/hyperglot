659e17767aa191aa557fa5228f7d9010546895c9
David Březina <david@rosettatype.com>
2024-08-28 19:35:54 +0200

    Updated the revise_references tool

--- tools/revise_references.py
+++ tools/revise_references.py
@@ -15,47 +15,53 @@ alvestrand = {
 }
 ethnologue = {
     "reference": "{name}. ({date}). In *Ethnologue*. SIL International. {url}",
-    "url": "https://www.ethnologue.com/language/{code}/"
+    "url": "https://www.ethnologue.com/language/{}/"
 }
 omniglot = {
     "reference": "Ager, Simon. ({date}) {name} language, alphabet, and pronunciation. *Omniglot*. {url}",
-    "url": "https://www.omniglot.com/writing/{lc_name}.htm"
+    "url": "https://www.omniglot.com/writing/{}.htm"
 }
 wiki = {
     "reference": "{name} language. ({date}). In *Wikipedia*. {url}",
 }
 
+# TODO: CLDR 
+
 today = datetime.today().strftime("%Y, %B %-d")
 paths = sorted(glob.glob("../lib/hyperglot/data/*.yaml"))
-i = 0
+total = 0
 for path in paths:
      code = os.path.basename(path).replace(".yaml", "").replace("_", "").lower()
      if code == "dflt":
          continue
-     i += 1
      with open(path, "rb") as f:
         data = yaml.load(f, Loader=yaml.Loader)
         if not data:
-            print(code, "some issue")
+            print("There was an issue opening: {}.yaml".format(code))
+            continue
+        if "sources" not in data:
             continue
         name = data["name"]
-        print(name)
         sources = []
-        for source in data["source"]:
+        changed = False
+        print("-", name)
+        for source in data["sources"]:
             # Alvestrand (add only when "Alvestrand" is present)
             if source == "Alvestrand":
                 sources.append(alvestrand["reference"])
             # Ethnologue (add only when "Ethnologue" is present, no URL check)
             elif source == "Ethnologue":
-                ethnologue_url = ethnologue["url"].format(code=code)
+                ethnologue_url = ethnologue["url"].format(code)
                 sources.append(ethnologue["reference"].format(name=name, date=today, url=ethnologue_url))
+                changed = True
             # Omniglot (add only when "Omniglot" is present and URL exists)
             elif source == "Omniglot":
                 sources.append("Omniglot")
-            #     omniglot_url = omniglot["url"].format(lc_name=name.lower())
+            #     omniglot_url = omniglot["url"].format(name.lower())
             #     response = requests.get(omniglot_url)
             #     if response.status_code == 200:
             #         sources.append(omniglot["reference"].format(name=name, date=today, url=omniglot_url))
+            #     changed = True
             #     else:
             #         # print("'%s' does not exist" % omniglot_url)
             #         sources.append("Omniglot  # REVIEW")
@@ -66,14 +72,15 @@ for path in paths:
                     res = wikipedia.page("{} language".format(name))
                     wiki_url = res.url + "?oldid=" + str(res.revision_id)
                     sources.append(wiki["reference"].format(name=name, date=today, url=wiki_url))
+                    changed = True
                 except Exception as exp:
                     sources.append("Wikipedia  # REVIEW")
                     # print(exp)
             else:
                 sources.append(source + "  # REVIEW")
-            if "source" in data:
-                del data["source"]
             data["sources"] = sources
+            if changed:
+                total += 1
      with open(path, "w", encoding="utf-8") as f:
         try:
             yaml.safe_dump(data, f, allow_unicode=True,
@@ -81,6 +88,5 @@ for path in paths:
         except yaml.YAMLError as exc:
             logging.warning(exc)
      del data
-     if i >= 5:
-         break
+print("Total languages affected:", total)
 
