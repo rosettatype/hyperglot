97dd03300484f9093f7a34545097e9b2eb18fba6
David Březina <david@rosettatype.com>
2024-08-29 10:48:29 +0200

    Added Lahnda macrolanguage (Punjab/Pakistan, Afghanistan)

--- tools/revise_references.py
+++ tools/revise_references.py
@@ -9,32 +9,26 @@ import os
 import requests
 import wikipedia
 import yaml
+from bs4 import BeautifulSoup
 
-alvestrand = {
-    "reference": "Alvestrand, Harald. (1995) *Characters and character sets for various languages.* Retrieved on September 6, 2021 from https://www.alvestrand.no/ietf/lang-chars.txt"
-}
-ethnologue = {
-    "reference": "{name}. ({date}). In *Ethnologue*. SIL International. {url}",
-    "url": "https://www.ethnologue.com/language/{}/"
-}
 omniglot = {
     "reference": "Ager, Simon. ({date}) {name} language, alphabet, and pronunciation. *Omniglot*. {url}",
     "url": "https://www.omniglot.com/writing/{}.htm"
 }
 wiki = {
-    "reference": "{name} language. ({date}). In *Wikipedia*. {url}",
+    "reference": "{name}. ({date}). In *Wikipedia*. {url}",
 }
 
-# TODO: CLDR 
+# TODO: CLDR, REVIEW, Evertype, Omniglot, ominglot, Unicode specification
 
 today = datetime.today().strftime("%Y, %B %-d")
 paths = sorted(glob.glob("../lib/hyperglot/data/*.yaml"))
 total = 0
 for path in paths:
-     code = os.path.basename(path).replace(".yaml", "").replace("_", "").lower()
-     if code == "dflt":
-         continue
-     with open(path, "rb") as f:
+    code = os.path.basename(path).replace(".yaml", "").replace("_", "").lower()
+    if code == "dflt":
+        continue
+    with open(path, "rb") as f:
         data = yaml.load(f, Loader=yaml.Loader)
         if not data:
             print("There was an issue opening: {}.yaml".format(code))
@@ -42,21 +36,13 @@ for path in paths:
         if "sources" not in data:
             continue
         name = data["name"]
-        sources = []
+        sources = [] 
         changed = False
         print("-", name)
         for source in data["sources"]:
-            # Alvestrand (add only when "Alvestrand" is present)
-            if source == "Alvestrand":
-                sources.append(alvestrand["reference"])
-            # Ethnologue (add only when "Ethnologue" is present, no URL check)
-            elif source == "Ethnologue":
-                ethnologue_url = ethnologue["url"].format(code)
-                sources.append(ethnologue["reference"].format(name=name, date=today, url=ethnologue_url))
-                changed = True
             # Omniglot (add only when "Omniglot" is present and URL exists)
-            elif source == "Omniglot":
-                sources.append("Omniglot")
+            # if source == "Omniglot":
+            #     sources.append("Omniglot")
             #     omniglot_url = omniglot["url"].format(name.lower())
             #     response = requests.get(omniglot_url)
             #     if response.status_code == 200:
@@ -66,27 +52,35 @@ for path in paths:
             #         # print("'%s' does not exist" % omniglot_url)
             #         sources.append("Omniglot  # REVIEW")
             # Wikipedia (always add, remove "Wikipedia")
-            elif source == "Wikipedia":
-                # use permanent link as an url
+            if "wikipedia" in source:
+                # Arbëreshë Albanian language. (2024, August 28). In *Wikipedia*. https://en.wikipedia.org/wiki/Arb%C3%ABresh_language?oldid=1239219170
+                url = source.split(". ")[-1].strip()
                 try:
-                    res = wikipedia.page("{} language".format(name))
-                    wiki_url = res.url + "?oldid=" + str(res.revision_id)
-                    sources.append(wiki["reference"].format(name=name, date=today, url=wiki_url))
-                    changed = True
+                    #res = wikipedia.page(title)
+                    r = requests.get(url)
+                    #wiki_url = res.url + "?oldid=" + str(res.revision_id)
                 except Exception as exp:
-                    sources.append("Wikipedia  # REVIEW")
-                    # print(exp)
+                    # sources.append("Wikipedia  # REVIEW")
+                    print("ERROR at", url)
+                parsed_html = BeautifulSoup(r.text, "html.parser")
+                name = parsed_html.find(id="firstHeading").text
+                date = parsed_html.find(id="mw-revision-date").text.split(", ")[-1].split(" ")  # 8 August 2024
+                date = "%s, %s %s" % (date[2], date[1], date[0])
+                reference = wiki["reference"].format(name=name, date=date, url=url)
+                if reference != source:
+                    sources.append(wiki["reference"].format(name=name, date=date, url=url))
+                    changed = True
             else:
-                sources.append(source + "  # REVIEW")
-            data["sources"] = sources
-            if changed:
-                total += 1
-     with open(path, "w", encoding="utf-8") as f:
+                sources.append(source)
+        if changed:
+            total += 1
+        data["sources"] = sources
+    with open(path, "w", encoding="utf-8") as f:
         try:
             yaml.safe_dump(data, f, allow_unicode=True,
-                           default_flow_style=False)
+                            default_flow_style=False)
         except yaml.YAMLError as exc:
             logging.warning(exc)
-     del data
+    del data
 print("Total languages affected:", total)
 
